2276a1baaf9469c45509a1ea3998ef879b90a09d
Smooth Operator <bsaikou1990@gmail.com>
2023-08-15 19:40:29 -0400

    functional get and post routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,32 +1,57 @@
 // Api router
 const api = require('express').Router();
-const fs = require('fs').promises;
+const fs = require('fs');
+const path = require('path')
 // Generates unique ID for each note
 const uuid = require('../helpers/uuid.js');
 
 api.get('/api/notes', (req, res) => {
     console.info(`${req.method} request received for new note`);
-    let note = fs.readFile('./db/notes.json', 'UTF-8')
-    .then(res.json(JSON.parse(note)));
+    fs.readFile(path.join(__dirname, '../db/notes.json'), 'UTF-8', (error, data) => {
+        if (error) {
+            res.json(error)
+        } else {
+            res.json(JSON.parse(data));
+        }
+    })
 });
 
 api.post('/api/notes', (req, res) => {
-    let note = JSON.parse(fs.readFile('./db/notes.json', 'UTF-8'));
-    const { title } = req.body.title;
-    const { text } = req.body.text;
+    fs.readFile(path.join(__dirname, '../db/notes.json'), 'UTF-8', (error, data) => {
+        if (error) {
+            res.json(error)
+        } else {
+            const notes = JSON.parse(data);
+            const { title, text } = req.body;
 
-    if (title && text) {
-        const newNote = {
-            title,
-            text,
-            note_id: uuid(),
-        };
-        note.push(newNote);
-        fs.writeFile('./db/notes.json', JSON.stringify(note));
-        res.json('Successfully added new note');
-    } else {
-        res.status(400).send('Error creating new note');
-    }
+            if (title && text) {
+                const newNote = {
+                    title,
+                    text,
+                    note_id: uuid(),
+                };
+                notes.push(newNote);
+                fs.writeFile(path.join(__dirname, '../db/notes.json'), JSON.stringify(notes), (error) => {
+                    if (error) {
+                        res.status(400).send('Error creating new note');
+                    } else {
+                        res.json('Successfully added new note');
+                    }
+                });
+            }
+        }
+    })
 });
+api.delete('/api/notes/:id',(req, res) => {
+    fs.readFile(path.join(__dirname, '../db/notes.json'), 'UTF-8', (error, data) => {
+        if (error) {
+            res.json(error)
+        } else {
+            const notes = JSON.parse(data);
+            const { title, text } = req.body;
+            console.log(req.params.id)
+        }
+    })         
+})
 
 module.exports = api;
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,14 +2,14 @@
 const html = require('express').Router();
 const path = require('path');
 
-// Getting route for homepage
-html.get('*', (req, res) =>
-res.sendFile(path.join(__dirname, '../public/index.html'))
-);
 console.log(__dirname)
 // Getting route for notes.html
 html.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
+res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
+// Getting route for homepage
+html.get('/', (req, res) =>
+res.sendFile(path.join(__dirname, '../public/index.html'))
+);
 module.exports = html;
